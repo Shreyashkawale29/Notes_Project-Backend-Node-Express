e255f90507c52967623e7286e472a7c8f1180212
Shreyash Kawale <kawalesn01@gmail.com>
2025-12-05 19:17:47 +0530

    Add New feactures

--- index.js
+++ index.js
@@ -9,11 +9,49 @@ app.use(express.json()); // Parsers
 app.use(express.urlencoded({ extend: true })); // Both are used for form handling
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
+app.get(`/`, function (req, res) {
   fs.readdir(`./files`, function (err, files) {
     console.log(files);
+    res.render("index.ejs", { files: files });
   });
-  res.render("index.ejs");
+});
+
+app.get(`/file/:filename`, function (req, res) {
+  fs.readFile(
+    `./files/${req.params.filename}`,
+    "utf-8",
+    function (err, filedata, title) {
+      console.log(filedata);
+      res.render("show.ejs", {
+        filename: req.params.filename,
+        title: title,
+        filedata: filedata,
+      });
+    }
+  );
+});
+
+app.get(`/edit/:filename`, function (req, res) {
+  res.render(`edit.ejs`, { filename: req.params.filename });
+});
+
+app.post(`/create`, function (req, res) {
+  console.log(req.body);
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    function (err) {
+      res.redirect("/");
+    }
+  );
+});
+
+app.post(`/edit`, function (req, res) {
+  console.log(req.body);
+  fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`, function(err) {
+      res.redirect("/");
+    }
+  );
 });
 
 // app.get("/profile/:username", function (req, res) {
